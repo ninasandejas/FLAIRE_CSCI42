68273bbae0b82e79fab15c0492ba81938342fa9f
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2025-03-07 18:37:30 +0800

    Edited settings to use PostgreSQL

--- flaire/flaire/settings.py
+++ flaire/flaire/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'flaire.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'flaire_database'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 

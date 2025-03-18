1bc35b3328d96cd363697edd0a347830410a589f
Zoe Ongkiko <146398841+ZoeOngkiko@users.noreply.github.com>
2025-03-18 20:17:20 +0800

    set up homepage and closet app with views, URL routing, and styled navigation bar

--- flaire/flaire/urls.py
+++ flaire/flaire/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.shortcuts import render
+from django.urls import include, path
+
+
+def home(request):
+    return render(request, "home.html", {"active_tab": "home"})
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("closet/", include("closet.urls", namespace="closet")),
+    path("", home, name="home"),
 ]

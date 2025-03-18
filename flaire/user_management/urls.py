91f312909f605380518e994e57f1b4461cd991fb
heartmethody13 <heart.castanares@student.ateneo.edu>
2025-03-18 23:28:55 +0800

    added a few styling to signup page

--- flaire/user_management/urls.py
+++ flaire/user_management/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import UserLoginView, UserCreateView, PlaceholderView
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'user_management'
 

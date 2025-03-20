bcb6539f014ec3aeb2bcbb78a0fa74f23bcf2a08
Zoe Ongkiko <146398841+ZoeOngkiko@users.noreply.github.com>
2025-03-20 17:15:56 +0800

    uploaded HD icon images and modified css

--- flaire/closet/views.py
+++ flaire/closet/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 
 def closet(request):
     return render(request, "closet/outfit-builder.html", {"active_tab": "closet"})
-
-
-def home(request):
-    return render(request, "home.html", {"active_tab": "home"})

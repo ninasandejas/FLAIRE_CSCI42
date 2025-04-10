efd871b54696236857b30a4f1e03186a8e134462
heartmethody13 <heart.castanares@student.ateneo.edu>
2025-04-11 06:02:22 +0800

    fixed showroom models to properly manage many to many fields

--- flaire/showrooms/admin.py
+++ flaire/showrooms/admin.py
@@ -1,28 +1,34 @@
 from django import forms
 from django.contrib import admin
 
-from .models import Showroom, Profile, Outfit
+from .models import *
 
 
-# class ShowroomAdminForm(forms.ModelForm):
-#     class Meta:
-#         model = Showroom
-#         fields = '__all__'
-
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.fields['collaborators'].queryset = Profile.objects.all()
-#         self.fields['outfits'].queryset = Outfit.objects.all()
+class ShowroomCollaboratorInline(admin.TabularInline):
+    model = ShowroomCollaborator
+    extra = 1
+    # autocomplete_fields = ['outfit']
 
 
-# TODO: fix the pre-selection of all outfits and all users currently in the system for the collaborators and outfits
+class ShowroomOutfitInline(admin.TabularInline):
+    model = ShowroomOutfit
+    extra = 1
+    # autocomplete_fields = ['outfit']
+
+
+class ShowroomFollowerInline(admin.TabularInline):
+    model = ShowroomFollower
+    extra = 1
+    # autocomplete_fields = ['outfit']
+
+
 class ShowroomAdmin(admin.ModelAdmin):
     model = Showroom
-    # form = ShowroomAdminForm
     list_display = ('title', 'owner', 'is_public', 'date_created')
     list_filter = ('is_public', 'date_created')
     search_fields = ('title', 'owner__user__username')
     readonly_fields = ("date_created", "date_updated")
+    inlines = [ShowroomCollaboratorInline, ShowroomOutfitInline, ShowroomFollowerInline]
     ordering = ("-date_created",)
 
     # filter_horizontal = ('collaborators', 'outfits')

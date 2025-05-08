efa62bf676aa2c0f5846aae99aab9f353637f346
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2025-05-08 19:10:28 +0800

    Fixed following to display outfits and outfit post modal.

--- flaire/static/js/social/following.js
+++ flaire/static/js/social/following.js
@@ -1,42 +1,90 @@
 document.addEventListener("DOMContentLoaded", () => {
   const outfitTab = document.getElementById("following-outfits-tab");
   const showroomTab = document.getElementById("following-showrooms-tab");
-  const grid = document.getElementById("following-grid");
+  const outfitsGrid = document.getElementById("outfits-grid");
+  const showroomsGrid = document.getElementById("showrooms-grid");
 
-  const loadOutfits = () => {
-    fetch("/social/following-outfits/")
-      .then(response => response.json())
-      .then(data => {
-        grid.innerHTML = "";
-        data.images.forEach(image => {
-          const img = document.createElement("img");
-          img.src = image.url;
-          img.classList.add("grid-image");
-          img.dataset.outfitId = image.id;
-          grid.appendChild(img);
-        });
-      });
+  const activateTab = (tabToActivate) => {
+    [outfitTab, showroomTab].forEach(tab => tab.classList.remove("active"));
+    tabToActivate.classList.add("active");
   };
 
-  const loadShowrooms = () => {
-    fetch("/social/following-showrooms/")
-      .then(response => response.json())
-      .then(data => {
-        grid.innerHTML = "";
-        data.showrooms.forEach(showroom => {
-          const div = document.createElement("div");
-          div.classList.add("showroom-card");
-          div.innerHTML = `
-            <img src="${showroom.url}" class="grid-image" />
-            <p>${showroom.title}</p>
-          `;
-          grid.appendChild(div);
-        });
-      });
-  };
+  outfitTab.addEventListener("click", () => {
+    activateTab(outfitTab);
+    outfitsGrid.classList.remove("hidden-grid");
+    showroomsGrid.classList.add("hidden-grid");
+    outfitsGrid.innerHTML = "";
+    loadOutfits();
+  });
 
-  outfitTab.addEventListener("click", loadOutfits);
-  showroomTab.addEventListener("click", loadShowrooms);
+  showroomTab.addEventListener("click", () => {
+    activateTab(showroomTab);
+    outfitsGrid.classList.add("hidden-grid");
+    showroomsGrid.classList.remove("hidden-grid");
+    showroomsGrid.innerHTML = "";
+    loadShowrooms();
+  });
 
+  // Load outfits by default
   loadOutfits();
+
+  function loadOutfits() {
+    fetch(`/social/following-outfits/`)
+      .then(res => res.json())
+      .then(data => {
+        outfitsGrid.innerHTML = "";
+        data.images.forEach(outfit => {
+          const img = document.createElement("img");
+          img.src = outfit.url;
+          img.alt = `Outfit ${outfit.id}`;
+          img.classList.add("outfit-image");
+          img.dataset.outfitId = outfit.id;
+
+          const wrapper = document.createElement("div");
+          wrapper.classList.add("post-wrapper");
+          wrapper.appendChild(img);
+          outfitsGrid.appendChild(wrapper);
+        });
+
+        // Modal click
+        outfitsGrid.querySelectorAll(".outfit-image").forEach(img => {
+          img.addEventListener("click", (e) => {
+            const outfitId = e.currentTarget.dataset.outfitId;
+            fetch(`/closet/${outfitId}/`)
+              .then(res => res.json())
+              .then(data => showOutfitPostModal(data));
+          });
+        });
+      })
+      .catch(err => console.error("Error loading following outfits:", err));
+  }
+
+  function loadShowrooms() {
+    fetch(`/social/following-showrooms/`)
+      .then(res => res.json())
+      .then(data => {
+        showroomsGrid.innerHTML = "";
+        data.showrooms.forEach(showroom => {
+          const img = document.createElement("img");
+          img.src = showroom.url;
+          img.alt = showroom.title;
+          img.classList.add("showroom-thumbnail");
+          img.dataset.showroomId = showroom.id;
+
+          const wrapper = document.createElement("div");
+          wrapper.classList.add("post-wrapper");
+          wrapper.appendChild(img);
+          showroomsGrid.appendChild(wrapper);
+        });
+
+        // Click handler to visit showroom page
+        showroomsGrid.querySelectorAll(".showroom-thumbnail").forEach(img => {
+          img.addEventListener("click", (e) => {
+            const showroomId = e.currentTarget.dataset.showroomId;
+            window.location.href = `/showroom/${showroomId}/`;
+          });
+        });
+      })
+      .catch(err => console.error("Error loading following showrooms:", err));
+  }
 });

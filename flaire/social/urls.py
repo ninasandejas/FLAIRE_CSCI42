77d229f9b5db22170edb2fc1a25cacfc01356939
Zoe Ongkiko <146398841+ZoeOngkiko@users.noreply.github.com>
2025-05-10 10:10:43 +0800

    fixed css problem from explore/following merge

--- flaire/social/urls.py
+++ flaire/social/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+
 from .views import *
 
 app_name = "social"
@@ -9,8 +10,20 @@ urlpatterns = [
     path("explore/", explore, name="explore"),
     path("following/", following, name="following"),
     path("explore-outfits/", ExploreOutfitsGridView.as_view(), name="explore-outfits"),
-    path("explore-showrooms/", ExploreShowroomsGridView.as_view(), name="explore-showrooms"),
-    path("following-outfits/", FollowingOutfitsGridView.as_view(), name="following-outfits"),
-    path("following-showrooms/", FollowingShowroomsGridView.as_view(), name="following-showrooms"),
-    path('notifications/', views.fetch_notifications, name='notifs')
+    path(
+        "explore-showrooms/",
+        ExploreShowroomsGridView.as_view(),
+        name="explore-showrooms",
+    ),
+    path(
+        "following-outfits/",
+        FollowingOutfitsGridView.as_view(),
+        name="following-outfits",
+    ),
+    path(
+        "following-showrooms/",
+        FollowingShowroomsGridView.as_view(),
+        name="following-showrooms",
+    ),
+    path("notifications/", fetch_notifications, name="notifs"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
